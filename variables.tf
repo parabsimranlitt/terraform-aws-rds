40e0c01373041575683cdf1e4b5c043013e5aec9
parabsimranlitt <parabsimran.litt@hashicorp.com>
2024-01-23 13:49:27 -0600

    Add validation to password length

--- variables.tf
+++ variables.tf
@@ -9,6 +9,11 @@ variable "db_username" {
 variable "db_password" {
   description = "RDS root user password"
   sensitive   = true
+
+  validation {
+    condition     = length(var.db_password) >= 8
+    error_message = "The string must be at least 8 characters long."
+  }
 }
 variable "db_encrypted" {
 description = "Encrypt the database storage"
